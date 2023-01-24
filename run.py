6e7287240f5c690335aaebdd9342a58b4684c192
Al3xandruF <al3x.florea@gmail.com>
2023-01-24 18:49:29 +0000

    Collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
+from pprint import pprint
 import gspread
 from google.oauth2.service_account import Credentials
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -84,6 +86,22 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
 
     return surplus_data
+
+
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
 
 
 def main():
