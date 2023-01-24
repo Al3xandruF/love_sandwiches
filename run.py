af86dd34c1fbbf22f77666901f753523cc7a3ad3
Al3xandruF <al3x.florea@gmail.com>
2023-01-24 05:28:49 +0000

    Adjust validation to check that all values provided are integers

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -22,9 +22,9 @@ def get_sales_data():
     print("Example: 10, 20, 30, 40, 50, 60\n")
 
     data_str = input("Enter your data here: ")
-    
+
     sales_data = data_str.split(",")
-    print(sales_data)
+    validate_data(sales_data)
 
 
 def validate_data(values):
@@ -33,7 +33,8 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
-    try: 
+    try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
